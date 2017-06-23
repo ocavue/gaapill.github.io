2ba31122b93e459b8efa4e7842ca31b01a58ce01
songjia <songjia@xiaonongnv.com>
2017-06-23 17:22:04 +0800

    init

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+/**
+ * Created by songjia on 2017/6/22.
+ */
+module.exports = {
+    "plugins": [
+        "gm"
+    ],
+    "rules": {
+        "react/display-name": 0
+    },
+    "extends": ["plugin:gm/recommended"]
+};
